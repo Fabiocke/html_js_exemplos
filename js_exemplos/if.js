10b30f108a49f9fa21555ae38a4a6e1bce42f45a
Fábio <fabiomt92@hotmail.com>
2021-03-11 21:47:14 -0300

    css_html

---
+++ js_exemplos/if.js
@@ -0,0 +1,18 @@
+
+
+
+
+var vel = 50
+console.log(`A velo é ${vel}`)
+
+
+if (vel >60) {
+    console.log('Voce ultrapassou')
+} else {
+    console.log('Nao ultrapassou')
+}
+
+
+
+
+
---
+++ js_exemplos/switch.js
@@ -0,0 +1,17 @@
+
+x=2
+
+switch (x) {
+    case 4:
+        console.log('1')
+        break
+    
+    case 3:
+        console.log('2')
+        break
+    default:
+        console.log('aaa')
+}
+
+
+
